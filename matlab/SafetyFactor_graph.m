020986dabd44d43a6b470cadf8e938acec01a4ec
David Qiu <david@davidqiu.com>
2014-02-11 06:34:17 +0800

    Signed-off-by: David Qiu <david@davidqiu.com>

--- matlab/SafetyFactor_graph.m
+++ matlab/SafetyFactor_graph.m
@@ -3,5 +3,10 @@ hold on;
 for x = 0.5:0.01:16
     %y = SafetyFactor(x);
     y = SafetyFactor_Trans(500/x, 1)/2;
-    plot([x x], [y y]);
+    plot([x x], [y y], 'b');
+end
+for x = 0.5:0.01:8
+    %y = SafetyFactor(x);
+    y = SafetyFactor_Trans(1000/x, 1);
+    plot([x x], [y y], 'r');
 end
