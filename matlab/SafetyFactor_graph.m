e1d0c0c3564d63df1aa9838143e8e66ed182189d
Sun Xiaotin <1907147243@qq.com>
2014-02-11 05:51:15 +0800

    Signed-off-by: Sun Xiaotin <1907147243@qq.com>

--- matlab/SafetyFactor_graph.m
+++ matlab/SafetyFactor_graph.m
@@ -1,7 +1,7 @@
 clear;
 hold on;
-for x = 0:1:300%1500
-    y = SafetyFactor(x);
-    %y = SafetyFactor_Trans(x, 1);
+for x = 0:1:1500
+    %y = SafetyFactor(x);
+    y = SafetyFactor_Trans(x, 1);
     plot([x x], [y y]);
 end
